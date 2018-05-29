2c77699feca8c71565262bbfc1d564239a22d8c4
Jon Manning <jon@secretlab.com.au>
2018-05-29 15:44:13 +1000

    Add quest tracking example

---
+++ Working/Gameplay/Assets/Quests/Quest.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// BEGIN quest
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// A Quest stores information about a quest: its name, and its objectives.
+
+// CreateAssetMenu makes the Create Asset menu contain an entry that creates
+// a new Quest asset.
+[CreateAssetMenu(fileName = "New Quest", menuName = "Quest", order = 100)]
+public class Quest : ScriptableObject
+{
+
+    // Represents the status of objectives and quests
+    public enum Status {
+        NotYetComplete, // the objective or quest has not yet been completed
+        Complete, // the objective or quest has been successfully completed
+        Failed // the objective or quest has failed
+    }
+
+    // The name of the quest
+    public string questName;
+
+    // The list of objectives that form this quest
+    public List<Objective> objectives;
+
+    // Objectives are the specific tasks that make up a quest.
+    [System.Serializable]
+    public class Objective
+    {
+        // The visible name that's shown the player.
+        public string name = "New Objective";
+
+        // If true, this objective doesn't need to be complete in order for
+        // the quest to be considered complete.
+        public bool optional = false;
+
+        // If false, the objective will not be shown to the user if it's not
+        // yet complete. (It will be shown if it's Complete or Failed.)
+        public bool visible = true;
+
+        // The status of the objective when the quest begins. Usually this will
+        // be "not yet complete", but you might want an objective that starts
+        // as Complete, and can be Failed.
+        public Status initalStatus = Status.NotYetComplete;
+    }
+
+}
+
+#if UNITY_EDITOR
+// Draw a custom editor that lets you build the list of objectives.
+[CustomEditor(typeof(Quest))]
+public class QuestEditor : Editor {
+
+    // Called when Unity wants to draw the Inspector for a Quest.
+    public override void OnInspectorGUI()
+    {
+
+        // Ensure that the object we're displaying has had any pending changes 
+        // done.
+        serializedObject.Update();
+
+        // Draw the name of the quest.
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("questName"), new GUIContent("Name"));
+
+        // Draw a header for the list of objectives
+        EditorGUILayout.LabelField("Objectives");
+
+        // Get the property that contains the list of objectives
+        var objectiveList = serializedObject.FindProperty("objectives");
+
+        // Indent the objectives
+        EditorGUI.indentLevel += 1;
+
+        // For each objective in the list, draw an entry
+        for (int i = 0; i < objectiveList.arraySize; i++)
+        {
+            // Draw a single line of controls
+            EditorGUILayout.BeginHorizontal();
+
+            // Draw the objective itself (its name, and its flags)
+            EditorGUILayout.PropertyField(
+                objectiveList.GetArrayElementAtIndex(i), 
+                includeChildren: true
+            );
+
+            // Draw a button that moves the item up in the list
+            if (GUILayout.Button("Up", EditorStyles.miniButtonLeft, 
+                                 GUILayout.Width(25)))
+            {
+                objectiveList.MoveArrayElement(i, i - 1);
+            }
+
+            // Draw a button that moves the item down in the list
+            if (GUILayout.Button("Down", EditorStyles.miniButtonMid, 
+                                 GUILayout.Width(40)))
+            {
+                objectiveList.MoveArrayElement(i, i + 1);
+            }
+
+            // Draw a button that removes the item from the list
+            if (GUILayout.Button("-", EditorStyles.miniButtonRight, 
+                                 GUILayout.Width(25)))
+            {
+                objectiveList.DeleteArrayElementAtIndex(i);
+            }
+
+            EditorGUILayout.EndHorizontal();
+
+        }
+
+        // Remove the indentation
+        EditorGUI.indentLevel -= 1;
+
+        // Draw a button at adds a new objective to the list
+        if (GUILayout.Button("Add Objective"))
+        {
+            objectiveList.arraySize += 1;
+        }
+
+        // Save any changes
+        serializedObject.ApplyModifiedProperties();
+
+    }
+}
+
+#endif
+// END quest
+
---
+++ Working/Gameplay/Assets/Quests/QuestManager.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// BEGIN quest_manager
+// Represents the player's current progress through a quest.
+public class QuestStatus {
+
+    // The underlying data object that describes the quest.
+    public Quest questData;
+
+    // The map of objective identifiers.
+    public Dictionary<int, Quest.Status> objectiveStatuses;
+
+    // The constructor. Pass a Quest to this to set it up.
+    public QuestStatus(Quest questData)
+    {
+        // Store the quest info
+        this.questData = questData;
+
+        // Create the map of objective numbers to their status
+        objectiveStatuses = new Dictionary<int, Quest.Status>();
+
+        for (int i = 0; i < questData.objectives.Count; i += 1)
+        {
+            var objectiveData = questData.objectives[i];
+
+            objectiveStatuses[i] = objectiveData.initalStatus;
+        }
+    }
+
+    // Returns the state of the entire quest.
+    // If all non-optional objectives are complete, the quest is complete.
+    // If any non-optional objective is failed, the quest is failed.
+    // Otherwise, the quest is not yet complete.
+    public Quest.Status questStatus {
+        get {
+            
+            for (int i = 0; i < questData.objectives.Count; i += 1) {
+                
+                var objectiveData = questData.objectives[i];
+
+                // Optional objectives do not matter to the overall quest status
+                if (objectiveData.optional)
+                    continue;
+
+                var objectiveStatus = objectiveStatuses[i];
+
+                // this is a mandatory objective
+                if (objectiveStatus == Quest.Status.Failed)
+                {
+                    // if a mandatory objective is failed, the whole 
+                    // quest is failed
+                    return Quest.Status.Failed;
+                }
+                else if (objectiveStatus != Quest.Status.Complete)
+                {
+                    // if a mandatory objective is not yet complete,
+                    // the whole quest is not yet complete
+                    return Quest.Status.NotYetComplete;
+                }
+            }
+
+            // All mandatory objectives are complete, so this quest is complete
+            return Quest.Status.Complete;
+
+        }
+    }
+
+    // Returns a string containing the list of objectives, their statuses, and
+    // the status of the quest.
+    public override string ToString()
+    {
+        var stringBuilder = new System.Text.StringBuilder();
+
+        for (int i = 0; i < questData.objectives.Count; i += 1)
+        {
+            // Get the objective and its status
+            var objectiveData = questData.objectives[i];
+            var objectiveStatus = objectiveStatuses[i];
+
+            // Don't show hidden objectives that haven't been finished
+            if (objectiveData.visible == false &&
+                objectiveStatus == Quest.Status.NotYetComplete)
+            {
+                continue;
+            }
+
+            // If this objective is optional, display "(Optional)" after its name
+            if (objectiveData.optional)
+            {
+                stringBuilder.AppendFormat("{0} (Optional) - {1}\n",
+                                           objectiveData.name, 
+                                           objectiveStatus.ToString());
+            }
+            else
+            {
+                stringBuilder.AppendFormat("{0} - {1}\n", 
+                                           objectiveData.name,
+                                           objectiveStatus.ToString());
+            }
+
+        }
+
+        // Add a blank line followed by the quest status
+        stringBuilder.AppendLine();
+        stringBuilder.AppendFormat("Status: {0}", this.questStatus.ToString());
+
+        return stringBuilder.ToString();
+    }
+}
+
+// Manages a quest.
+public class QuestManager : MonoBehaviour {
+
+    // The quest that starts when the game starts.
+    [SerializeField] Quest startingQuest = null;
+
+    // A label to show the state of the quest in.
+    [SerializeField] UnityEngine.UI.Text objectiveSummary = null;
+
+    // Tracks the state of the current quest.
+    QuestStatus activeQuest;
+
+    // Start a new quest when the game starts
+    void Start () {
+
+        if (startingQuest != null)
+        {
+            StartQuest(startingQuest);
+        }
+    }   
+
+    // Begins tracking a new quest
+    public void StartQuest(Quest quest) {
+
+        activeQuest = new QuestStatus(quest);
+
+        UpdateObjectiveSummaryText();
+
+        Debug.LogFormat("Started quest {0}", activeQuest.questData.name);
+    }
+
+    // Updates the label that displays the status of the quest and 
+    // its objectives
+    void UpdateObjectiveSummaryText() {
+
+
+        string label;
+
+        if (activeQuest == null) {
+            label = "No active quest.";
+        } else {
+            label = activeQuest.ToString();
+        }
+
+        objectiveSummary.text = label;
+    }
+
+    // Called by other objects to indicate that an objective has changed status
+    public void UpdateObjectiveStatus(Quest quest, int objectiveNumber, Quest.Status status) {
+
+        if (activeQuest == null) {
+            Debug.LogError("Tried to set an objective status, but no quest is active");
+            return;
+        }
+
+        if (activeQuest.questData != quest) {
+            Debug.LogWarningFormat("Tried to set an objective status for " +
+                                   "quest {0}, but this is not the active " +
+                                   "quest. Ignoring.", quest.questName);
+            return;
+        }
+
+        // Update the objective status
+        activeQuest.objectiveStatuses[objectiveNumber] = status;
+
+        // Update the display label
+        UpdateObjectiveSummaryText();
+    }
+
+
+}
+// ENDquest_manager
