e33f9a5e79022d0958b75c694d3ab0449689f2c5
Jon Manning <jon@secretlab.com.au>
2018-05-31 16:04:41 +1000

    GUI party

---
+++ Working/GUI/Assets/ButtonClicked.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// BEGIN button_clicked
+public class ButtonClicked : MonoBehaviour {
+
+    // This will appear in the list of methods
+    public void ButtonWasClicked() {
+        Debug.Log("The button was clicked!");
+    }
+
+    // This will appear in the list of methods, and let you specify a parameter
+    public void ButtonWasClickedWithParameter(string parameter) {
+        string message = 
+            string.Format("The button was clicked: {0}", parameter);
+        
+        Debug.Log(message);
+    }
+
+    // This won't appear in the list of methods, because it's private
+    // (even though we didn't specify its protection level, because all class
+    // methods are private by default in C#)
+    void PrivateButtonWasClicked() {
+        Debug.Log("This won't run as the direct result of a button click!");
+    }
+
+}
+// END button_clicked
---
+++ Working/GUI/Assets/ListItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// BEGIN list_item
+public class ListItem : MonoBehaviour {
+
+    // The Text object that displays our label
+    [SerializeField] UnityEngine.UI.Text labelText;
+
+    // Expose a string; setting or getting this will set or get the text of
+    // the label.
+	public string Label
+    {
+        get
+        {
+            return labelText.text;
+        }
+        set
+        {
+            labelText.text = value;
+        }
+    }
+
+}
+// END list_item
