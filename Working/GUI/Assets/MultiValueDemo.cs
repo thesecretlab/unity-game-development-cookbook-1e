548e8cd1747dddc7ed970cf14cb34e2bb3873644
Jon Manning <jon@secretlab.com.au>
2018-06-01 17:00:29 +1000

    Property drawers

---
+++ Working/GUI/Assets/MultiValueDemo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// BEGIN multivaluedemo
+// A simple component to demo the MultiValueChooser property drawer.
+public class MultiValueDemo : MonoBehaviour {
+
+    [SerializeField] 
+    MultiValue multiValue = new MultiValue("One", "Two", "Three");
+}
+// END multivaluedemo
