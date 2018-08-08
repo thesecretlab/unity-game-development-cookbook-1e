b71b0cf731774f2578620ca9352ff28a4627d77a
Paris Buttfield-Addison <paris@paris.id.au>
2018-08-08 17:29:22 +1000

    More quaterrnions. ohgod.
    Former-commit-id: 69116e4a163c8490ddae0f72636b2ceb584c03c2

--- Working/Maths/Assets/Scenes/QuaternionDemo.cs
+++ Working/Maths/Assets/Scenes/QuaternionDemo.cs
@@ -21,30 +21,32 @@ public class QuaternionDemo : MonoBehaviour {
             // around the X, Y and Z axes, these angles are referred to as 
             // "Euler angles" (named after the mathmatician Leonhardt Euler,
             // whose name is pronounced "oiler", and not "yew-ler".)
-
+			// BEGIN quaternions1
             var rotation = Quaternion.Euler(90, 0, 0);
-
+			// END quaternions1
             // You can use this to rotate a point around the origin.
+			// BEGIN quaternions2
             var input = new Vector3(0, 0, 1);
 
             var result = rotation * input;
             // = (0, -1, 0)
-
+			// END quaternions2
             // The 'identity' quaternion is one that represents no rotation at
             // all.
+            // BEGIN quaternions3_identity
             var identity = Quaternion.identity;
-
+			// END quaternions3_identity
             // You can interpolate between two rotations using the Slerp method.
             // Slerp is short for "spherical linear interpolation", and is a way
             // to smoothly move between to rotations in a way that means that the
             // change in angle is constant at every step. This is better than
             // a linear interpolation of angles, in which the angles change at a 
             // non-constant rate.
-
+			// BEGIN quaternions4
             var rotationX = Quaternion.Euler(90, 0, 0);
 
             var halfwayRotated = Quaternion.Slerp(identity, rotationX, 0.5f);
-
+			// END quaternions4
             // Quaternions can be combined together. For example, to rotate
             // something around the Y axis and then around the X axis, you
             // multiply them (they're applied in reverse order):
