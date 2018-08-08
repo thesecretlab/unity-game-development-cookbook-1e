764980c839f3a77b44a0b8c4cc381aacfbaa87a9
Jon Manning <jon@secretlab.com.au>
2018-08-08 19:35:43 +1000

    Update tags in angles demo
    Former-commit-id: dd45928b05d51f4ebc978c2b4c1bffdb5a6edde9

--- Working/Maths/Assets/Scenes/AnglesDemo.cs
+++ Working/Maths/Assets/Scenes/AnglesDemo.cs
@@ -12,14 +12,18 @@ public class AnglesDemo : MonoBehaviour {
             // are done as degrees. As a refresher, there are 360 degrees in a
             // circle.
 
+            // BEGIN angles1
             transform.Rotate(90, 0, 0); // rotate 90 degrees - one quarter circle - 
                                         // around the X axis
+            // END angles1
 
             // However, several math functions work in radians. There are 2π 
             // radians in a circle.
 
+            // BEGIN angles2
             // The sine of pi radians (one half-circle) is zero
             Mathf.Sin(Mathf.PI);  // = 0
+            // END angles2
 
             // Degrees are a little more familiar to most people, but radians are
             // easier for calculations to work in. For this reason, most of the
@@ -27,11 +31,13 @@ public class AnglesDemo : MonoBehaviour {
 
             // You can convert from radians to degrees, and back again:
 
+            // BEGIN angles3
             // Converting 90 degrees to radians
             var radians = 90 * Mathf.Deg2Rad; // ~= 1.57 (π / 2)
 
             // Converting 2π radians to degrees
             var degrees = 2 * Mathf.PI * Mathf.Rad2Deg; // = 360
+            // END angles3
 
             // The dot product of two unit vectors is equal to the cosine of the
             // angle between them.
@@ -41,7 +47,9 @@ public class AnglesDemo : MonoBehaviour {
 
             // This means that you can find the angle between two vectors like this:
 
+            // BEGIN angles4
             var angle = Mathf.Acos(Vector3.Dot(Vector3.up, Vector3.left));
+            // END angles4
             // = π radians; convert it to degrees if you want to show that
             // to the user
 
